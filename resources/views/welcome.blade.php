5743cdf89a4f633e1a3963e9134209f211d77781
Sudarshan Dubaga <sudarshandubaga@gmail.com>
2022-05-01 09:17:01 +0530

    https changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -649,7 +649,7 @@ industry's standard dummy text ever since the 1500s,</div>
         </div>
     </footer>
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script src="{{ url('js/app.js') }}"></script>
 </body>
 </html>
