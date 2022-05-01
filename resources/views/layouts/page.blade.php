ec88369ed5492cd82db9cd6db27d6347c79ad82b
Sudarshan Dubaga <sudarshandubaga@gmail.com>
2022-05-01 15:42:34 +0530

    new css changes

---
+++ resources/views/layouts/page.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+ 
+@section('main_content')
+    <!-- Page Header Start -->
+    <section id="page-header">
+        <div class="container">
+            <h1></h1>
+        </div>
+    </section>
+    <!-- Page Header End -->
+@endsection
